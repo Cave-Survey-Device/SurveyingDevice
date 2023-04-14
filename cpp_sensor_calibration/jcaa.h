a5c38122f77e3869c486fe718ffa02a0d9d79636
christiankuhlmann <99804123+christiankuhlmann@users.noreply.github.com>
2023-04-14 19:18:20 +0100

    updated calibrations for matlab export

--- cpp_sensor_calibration/jcaa.h
+++ cpp_sensor_calibration/jcaa.h
@@ -11,23 +11,66 @@
 #include <random>
 #define _USE_MATH_DEFINES
 #include <math.h>
+#include "mag_cal.h"
+#include "data_generation.h"
 
 using namespace Eigen;
 
-const int N_samples = 100;
-
-struct input_vec
+struct StructUnknowns
 {
-    Matrix<float,3,N_samples> ya;   // Raw accelerometer measurements
-    Matrix<float,3,N_samples> ym;   // Raw magnetometer measurements
     Matrix3f Ha;
     Vector3f va;
-    Matrix<float,3,N_samples> f;    // Actual accelerometer measurements
+    Matrix<float,3,N_SAMPLES> f;    // Actual accelerometer measurements
     Matrix3f Hm;
     Vector3f vm;
-    Matrix<float,3,N_samples> m;    // Actual magnetometer measurements
+    Matrix<float,3,N_SAMPLES> m;    // Actual magnetometer measurements
     Matrix3f R;
     float d;
+
+    inline StructUnknowns operator+(StructUnknowns a) {
+        StructUnknowns out;
+        out.Ha = Ha + a.Ha;
+        out.va = va + a.va;
+        out.f = f + a.f;
+        out.Hm = Hm + a.Hm;
+        out.vm = vm + a.vm;
+        out.m = m + a.m;
+        out.R = R + a.R;
+        out.d = d + a.d;
+        return out;
+    }
+
+    inline StructUnknowns operator*(float val) {
+        StructUnknowns out;
+        out.Ha = Ha * val;
+        out.va = va * val;
+        out.f = f * val;
+        out.Hm = Hm * val;
+        out.vm = vm * val;
+        out.m = m * val;
+        out.R = R * val;
+        out.d = d * val;
+        return out;
+    }
+
+    inline StructUnknowns operator-(StructUnknowns a) {
+        StructUnknowns out;
+        out.Ha = Ha - a.Ha;
+        out.va = va - a.va;
+        out.f = f - a.f;
+        out.Hm = Hm - a.Hm;
+        out.vm = vm - a.vm;
+        out.m = m - a.m;
+        out.R = R - a.R;
+        out.d = d - a.d;
+        return out;
+    }
+};
+
+struct StructSamples
+{
+    Matrix<float,3,N_SAMPLES> ya;   // Raw accelerometer measurements
+    Matrix<float,3,N_SAMPLES> ym;   // Raw magnetometer measurements
 };
 
 MatrixXf kron(MatrixXf m1, MatrixXf m2)
@@ -40,55 +83,84 @@ MatrixXf kron(MatrixXf m1, MatrixXf m2)
     MatrixXf out(m*p, n*q);
     for(int i=0; i<m; i++)
     {
-        for(int j=0; i<n; i++) {
-            out.block(i*p,i*q,p,q) = m1(i,j) * m2;
+        for(int j=0; j<n; j++) {
+            out.block(i*p,j*q,p,q) = m1(i,j) * m2;
         }
     }
     return out;
 }
 
-Matrix3f dJ_Ha(input_vec X)
+// Cost function, J(x)
+float J(StructUnknowns X, StructSamples data)
 {
-    Matrix3f dJ_Ha;
+    float sum1 = 0;
+    float sum2 = 0;
+    float sum3 = 0;
+    Vector3f fk;
+    Vector3f mk;
+    Vector3f yak;
+    Vector3f ymk;
+
+    float probe;
+
+    for(int k=0;k<N_SAMPLES;k++)
+    {
+        fk = X.f.col(k);
+        yak = data.ya.col(k);
+        mk = X.m.col(k);
+        ymk = data.ym.col(k);
+
+        probe = (fk-X.Ha * yak + X.va).norm();
+
+        sum1 += pow((fk-X.Ha * yak + X.va).norm(),2) + pow(pow(fk.norm(),2) - 1,2);
+        sum2 += pow((mk-X.Hm * ymk + X.vm).norm(),2) + pow(pow(mk.norm(),2) - 1,2);
+        sum3 += pow(X.d-fk.transpose()*X.R*mk,2) + pow((X.R*X.R.transpose() - Matrix3f::Identity()).norm(),2) + pow(X.R.determinant()-1,2);
+    }
+    return sum1 + sum2 + sum3;
+}
+
+Matrix3f dJ_Ha(StructUnknowns X, StructSamples data)
+{
+    Vector<float,9> dJ_Ha;
     dJ_Ha.setZero();
     Vector3f yak;
     Vector3f fk;
 
-    for(int k=0; k<N_samples; k++)
+    for(int k=0; k<N_SAMPLES; k++)
     {
-        yak = X.ya.col(k);
+        yak = data.ya.col(k);
         fk = X.f.col(k);
         dJ_Ha += 2 * kron(yak,X.Ha*yak-fk-X.va);
     }
-    return dJ_Ha;
+    return dJ_Ha.reshaped(3,3);
 }
 
-Vector3f dJ_va(input_vec X)
+Vector3f dJ_va(StructUnknowns X, StructSamples data)
 {
     Vector3f dJ_va;
     dJ_va.setZero();
     Vector3f yak;
     Vector3f fk;
 
-    for(int k=0; k<N_samples; k++)
+    for(int k=0; k<N_SAMPLES; k++)
     {
-        yak = X.ya.col(k);
+        yak = data.ya.col(k);
         fk = X.f.col(k);
-        dJ_va += 2 * -X.Ha * yak + X.va + fk;
+        dJ_va += 2 * (-X.Ha * yak + X.va + fk);
     }
     return dJ_va;
 }
 
-Matrix<float,3,N_samples> dJ_f(input_vec X)
+Matrix<float,3,N_SAMPLES> dJ_f(StructUnknowns X, StructSamples data)
 {
-    Matrix<float,3,N_samples> dJ_f;
+    Matrix<float,3,N_SAMPLES> dJ_f;
     Vector3f yap;
     Vector3f fp;
     Vector3f mp;
 
-    for(int p=0; p<N_samples;p++)
+    for(int p=0; p<N_SAMPLES;p++)
     {
-        yap = X.ya.col(p);
+        yap = data.ya.col(p);
         fp = X.f.col(p);
         mp = X.m.col(p);
         dJ_f.block(0,p,3,1) = -2 * (X.Ha * yap + fp - X.va)
@@ -98,49 +170,49 @@ Matrix<float,3,N_samples> dJ_f(input_vec X)
     return dJ_f;
 }
 
-Matrix3f dJ_Hm(input_vec X)
+Matrix3f dJ_Hm(StructUnknowns X, StructSamples data)
 {
-    Matrix3f dJ_Hm;
+    Vector<float,9> dJ_Hm;
     dJ_Hm.setZero();
     Vector3f ymk;
     Vector3f mk;
 
-    for(int k=0; k<N_samples; k++)
+    for(int k=0; k<N_SAMPLES; k++)
     {
-        ymk = X.ym.col(k);
+        ymk = data.ym.col(k);
         mk = X.m.col(k);
         dJ_Hm += 2 * kron(ymk,X.Hm*ymk-mk-X.vm);
     }
-    return dJ_Hm;
+    return dJ_Hm.reshaped(3,3);
 }
 
-Vector3f dJ_vm(input_vec X)
+Vector3f dJ_vm(StructUnknowns X, StructSamples data)
 {
     Vector3f dJ_vm;
     dJ_vm.setZero();
     Vector3f mak;
     Vector3f mk;
 
-    for(int k=0; k<N_samples; k++)
+    for(int k=0; k<N_SAMPLES; k++)
     {
-        mak = X.ym.col(k);
+        mak = data.ym.col(k);
         mk = X.m.col(k);
-        dJ_vm += 2 * -X.Hm * mak + X.vm + mk;
+        dJ_vm += 2 * (-X.Hm * mak + X.vm + mk);
     }
     return dJ_vm;
 }
 
-Matrix<float,3,N_samples> dJ_m(input_vec X)
+Matrix<float,3,N_SAMPLES> dJ_m(StructUnknowns X, StructSamples data)
 {
-    Matrix<float,3,N_samples> dJ_f;
+    Matrix<float,3,N_SAMPLES> dJ_f;
     Vector3f ymp;
     Vector3f mp;
     Vector3f fp;
 
 
-    for(int p=0; p<N_samples;p++)
+    for(int p=0; p<N_SAMPLES;p++)
     {
-        ymp = X.ym.col(p);
+        ymp = data.ym.col(p);
         mp = X.m.col(p);
         fp = X.f.col(p);
         dJ_f.block(0,p,3,1) = -2 * (X.Hm * ymp + mp - X.vm)
@@ -150,12 +222,112 @@ Matrix<float,3,N_samples> dJ_m(input_vec X)
     return dJ_f;
 }
 
-Matrix3f dJ_R(input_vec X)
+Matrix3f dJ_R(StructUnknowns X, StructSamples data)
 {
+    Matrix3f dJ_R;
+    dJ_R.setZero();
+    Vector3f mk;
+    Vector3f fk;
+    for(int k=0;k<N_SAMPLES;k++)
+    {
+        mk = X.m.col(k);
+        fk = X.f.col(k);
+        dJ_R += -2*((X.d-fk.transpose()*X.R*mk) * kron(mk,fk)).reshaped(3,3);
+    }
+    dJ_R += 4* (X.R*X.R.transpose()*X.R - X.R)
+            + 2* (X.R.determinant()-1)*X.R.adjoint().transpose();
+
+    return dJ_R;
 }
 
-float dJ_d(input_vec X)
+float dJ_d(StructUnknowns X, StructSamples data)
 {
+    float dJ_d;
+    dJ_d = 0;
+    Vector3f mk;
+    Vector3f fk;
+    for(int k=0;k<N_SAMPLES;k++)
+    {
+        mk = X.m.col(k);
+        fk = X.f.col(k);
+        dJ_d += 2 * X.d - fk.transpose() * X.R * mk;
+    }
+    return dJ_d;
+}
+
+StructUnknowns jcaa(Matrix<float,3,N_SAMPLES> m_samples, Matrix<float,3,N_SAMPLES> a_samples)
+{
+    StructUnknowns X;
+    StructUnknowns grad_J;
+    StructSamples data;
+    data.ya = a_samples;
+    data.ym = m_samples;
+
+    // Calculate magnetometer initial parameters
+    Matrix<float, 3, 3> M;
+    Vector<float, 3> n;
+    float d;
+
+    Vector<float, 10> U = fit_ellipsoid(m_samples);
+    M << U[0], U[5], U[4], U[5], U[1], U[3], U[4], U[3], U[2];
+    n << U[6], U[7], U[8];
+    d = U[9];
+
+    Vector<float, 12> out = calculate_transformation(M, n, d);
+
+    // Calibration matrix (T^-1)
+    Matrix<float, 3, 3> Hm;
+    Hm << out[0], out[1], out[2], out[3], out[4], out[5], out[6], out[7], out[8];
+    // Bias vector
+    Vector<float, 3> b;
+    b << out[9], out[10], out[11];
+
+    // Set initial guess for magnetometer
+    X.Hm = Hm;
+    X.vm = Hm * b;
+
+    // Set initial guess for accelerometer
+    X.Ha = Matrix3f::Identity();
+    X.va = Vector3f::Zero();
+
+    // Set other initial guesses
+    X.R = Matrix3f::Identity();
+    X.d = sin(Deg2Rad(54));
+
+    X.f = data.ya;
+    X.m = Hm * (data.ym.colwise() - b);
+
+    // Gradient descent
+    float cost = J(X,data);
+    float alpha = 0.75;
+    float t  = 10;
+    while (cost > 0.1*N_SAMPLES)
+    {
+        // Calculate dJ
+        grad_J.Ha = dJ_Ha(X, data);
+        grad_J.va = dJ_va(X, data);
+        grad_J.f = dJ_f(X, data);
+        grad_J.Hm = dJ_Hm(X, data);
+        grad_J.vm = dJ_vm(X, data);
+        grad_J.m = dJ_m(X, data);
+        grad_J.R = dJ_R(X, data);
+        grad_J.d = dJ_d(X, data);
+
+        // Find scale over which to operate gradient descent at a given step
+        t = 1;
+        while (J(X, data) < J(X - grad_J*t, data))
+        {
+            t = t * alpha;
+        }
+
+        X = X - grad_J * t;
+        cost = J(X,data);
+        if(t < 0.0000001)
+        {
+            break;
+        }
+    }
+    return X;
 
 }
 
--- cpp_sensor_calibration/main.cpp
+++ cpp_sensor_calibration/main.cpp
@@ -2,6 +2,7 @@
 #include "mag_cal.h"
 #include "data_generation.h"
 #include "include/gnuplot-iostream.h"
+#include "jcaa.h"
 #include <format>
 
 using namespace Eigen;
@@ -21,20 +22,21 @@ int main() {
 
     // Define error parameters
     Matrix3f Tm;
-    Tm <<  0.7, -0.8, 0.4,
-            1.1, 0.3, -0.1,
-            -0.3, 0.6, 0.7;
+    Tm <<  0.462,-0.0293,-0.037,
+    0.0686,0.4379,0.0303,
+    0.0427,-0.0336,0.4369;
 
     Vector3f hm;
-    hm << 0.5,1.7,2.6;
+    hm << -0.176,0.2214,0.0398;
 
     Matrix3f Ta;
-    Ta <<  0.7, -0.8, 0.4,
-            1.1, 0.3, -0.1,
-            -0.3, 0.6, 0.7;
+    Ta <<  9.77,0.0018,-0.030,
+    0.0019,9.7032,-0.0011,
+    -0.0087, -0.0013,9.6927;
+    Ta = Ta * 0.1;
 
     Vector3f ha;
-    ha << 0.5,1.7,2.6;
+    ha << -0.01472,-0.0011,-0.01274;
 
     // Generate sensor data
     Vector3f mag_vec = {1.,0.,0.};
@@ -51,45 +53,62 @@ int main() {
     writeToCSVfile("accel_samples.txt", accel_samples.transpose());
 
 
-    // Create initial guess for magnetometer calibration
+    // Declare variables
     Matrix<float, 3, 3> M;
     Vector<float, 3> n;
     float d;
+    Vector<float, 10> U;
+    Matrix<float, 3, 3> R;
+    Vector<float, 3> b;
 
-    Vector<float, 10> U = fit_ellipsoid(mag_samples);
+    // Calculate magnetometer calibration
+    U = fit_ellipsoid(mag_samples);
     M << U[0], U[5], U[4], U[5], U[1], U[3], U[4], U[3], U[2];
     n << U[6], U[7], U[8];
     d = U[9];
+    Vector<float, 12> mag_transformation = calculate_transformation(M, n, d);
 
-    Vector<float, 12> out = calculate_transformation(M, n, d);
-
-    // Calibration matrix (T^-1)
-    Matrix<float, 3, 3> R;
-    R << out[0], out[1], out[2], out[3], out[4], out[5], out[6], out[7], out[8];
-    // Bias vector
-    Vector<float, 3> b;
-    b << out[9], out[10], out[11];
-
+    R << mag_transformation[0], mag_transformation[1], mag_transformation[2], mag_transformation[3], mag_transformation[4], mag_transformation[5], mag_transformation[6], mag_transformation[7], mag_transformation[8];
+    b << mag_transformation[9], mag_transformation[10], mag_transformation[11];
     MatrixXf mag_corrections = R * (mag_samples.colwise() - b);
 
-    // MAG GNU PLOTTING
-    Gnuplot gp;
+    // Calculate accelerometer calibration
+    U = fit_ellipsoid(mag_samples);
+    M << U[0], U[5], U[4], U[5], U[1], U[3], U[4], U[3], U[2];
+    n << U[6], U[7], U[8];
+    d = U[9];
+    Vector<float, 12> accel_transformation = calculate_transformation(M, n, d);
 
-    auto plots = gp.splotGroup();
-    plots.add_plot1d_colmajor(mag_true_data, "with points title 'True Vector'");
-    plots.add_plot1d_colmajor(mag_samples, "with points title 'Samples'");
-    plots.add_plot1d_colmajor(mag_corrections, "with points title 'Corrected data'");
-    gp << plots;
+    R << mag_transformation[0], mag_transformation[1], mag_transformation[2], mag_transformation[3], mag_transformation[4], mag_transformation[5], mag_transformation[6], mag_transformation[7], mag_transformation[8];
+    b << mag_transformation[9], mag_transformation[10], mag_transformation[11];
+    MatrixXf accel_corrections = R * (mag_samples.colwise() - b);
 
+    writeToCSVfile("accel_corrections.txt", accel_corrections.transpose());
+    writeToCSVfile("mag_corrections.txt", mag_corrections.transpose());
 
-    // Run joint accelerometer and magnetometer calibration and alignment
-
-
-#ifdef _WIN32
-    // For Windows, prompt for a keystroke before the Gnuplot object goes out of scope so that
-    // the gnuplot window doesn't get closed.
-    std::cout << "Press enter to exit." << std::endl;
-    std::cin.get();
-#endif
+//    // Run joint accelerometer and magnetometer calibration and alignment
+//    StructUnknowns correction_parameters = jcaa(mag_samples, accel_samples);
+//    MatrixXf mag_corrections = correction_parameters.R * correction_parameters.Hm * (mag_samples.colwise() - correction_parameters.Hm.inverse()*correction_parameters.vm);
+//    MatrixXf accel_corrections = correction_parameters.Ha * (accel_samples.colwise() - correction_parameters.Ha.inverse()*correction_parameters.va);
+//
+//    writeToCSVfile("mag_partial_corrections.txt", mag_partial_corrections.transpose());
+//    writeToCSVfile("accel_corrections.txt", accel_corrections.transpose());
+//    writeToCSVfile("mag_corrections.txt", mag_corrections.transpose());
+//
+//
+//    // MAG GNU PLOTTING
+//    Gnuplot gp;
+//    auto plots = gp.splotGroup();
+//    plots.add_plot1d_colmajor(mag_true_data, "with points title 'True Vector'");
+//    plots.add_plot1d_colmajor(mag_samples, "with points title 'Samples'");
+//    plots.add_plot1d_colmajor(mag_corrections, "with points title 'Corrected data'");
+//    gp << plots;
+//
+//#ifdef _WIN32
+//    // For Windows, prompt for a keystroke before the Gnuplot object goes out of scope so that
+//    // the gnuplot window doesn't get closed.
+//    std::cout << "Press enter to exit." << std::endl;
+//    std::cin.get();
+//#endif
     return 0;
 }

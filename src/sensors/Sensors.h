35c2b41f6a06eb5174d7bb24089a1e4eb6ec9940
christiankuhlmann <99804123+christiankuhlmann@users.noreply.github.com>
2023-04-30 17:58:09 +0100

    Added Doxygen comments

--- src/sensors/Sensors.h
+++ src/sensors/Sensors.h
@@ -51,13 +51,14 @@ public:
     virtual float GetMeasurement()=0;
 
     // Toggle laser
-    virtual void ToggleLaser()=0;
-
-    virtual void EnableLaser()=0;
-     
-    virtual void DisableLaser()=0;
+    virtual void ToggleLaser(bool mode)=0;
 };
 
+/**
+ * @brief The sensorhandler class functions as a manager for all connected sensors. This includes a magnetometer, accelerometer, and laser distance sensor. SensorHandler requires implementations of the generic class InertialSensor to connect it to a magnetometer and an accelerometer. Additionally, an implementation of LaserSensor is required to add connection to a laser distance sensor.
+ * 
+ * This class is used to handle ALL sensor functions. This includes calibration, alignment, getting readings, etc.
+ */
 class SensorHandler
 {
 protected:
@@ -71,42 +72,104 @@ protected:
     float alignment_progress;
 
 public:
+    /**
+     * @brief Construct a new SensorHandler object with a magnetometer, accelerometer, and laser sensor
+     * 
+     * @param acc Pointer to the connected accelerometer object
+     * @param mag Pointer to the connected magnetometer object
+     * @param las Pointer to the connected laser sensor object
+     */
     SensorHandler(InertialSensor* acc, InertialSensor* mag, LaserSensor* las);
+
+    /**
+     * @brief Construct a new SensorHandler object with a magnetometer and accelerometer
+     * 
+     * @param acc Pointer to the connected accelerometer object
+     * @param mag Pointer to the connected magnetometer object
+     */
     SensorHandler(InertialSensor* acc, InertialSensor* mag);
+
+    /**
+     * @brief Default SensorHandler constructor
+     * 
+     */
     SensorHandler();
 
-    // Takes a measurement using all 3 sensors. Value returned is Vector3d(Heading, Inclination, Distace)
+    // 
+    /**
+     * @brief Takes a measurement using all 3 sensors. Value returned is Vector3d(Heading, Inclination, Distace)
+     * 
+     * @return Vector3f Heading [deg], Inclination [deg], Distance [m]
+     */
     Vector3f get_measurement();
 
-    // Calibrate the inertial sensors - accelerometer and magnetometer
+ 
+    /**
+     * @brief Calibrate the inertial sensors - accelerometer and magnetometer. This runs a one-off calibration using the stored calibration data in each InertialSensor bject respectively.
+     */
     void CalibrateInertial();
 
-    // llign the inrtial sensors
+    /**
+     * @brief Runs a one-off alignment of the inertial sensors using the stored calibration data in each InertialSensor respectively.
+     */
     void AlignInertial();
 
-    // Align inertial sensor with laser rangefinder
+    /**
+     * @brief Runs a one-off alignment of the laser and inertial sensors using the stored calibration data in each alignment_data respectively.
+     */
     void AlignLaser();   
 
-    // Collects alignemnt data ready for alignment process
+    /**
+     * @brief Collects one shot of alignment data.
+     * 
+     * @return true if all alignment shots HAVE been collected
+     * @return false if all alignment shots HAVE NOT been collected
+     */
     bool CollectAlignmentData();   
 
-    // Collect 1 sample of data for inertial sensors as part of the calibration procedure
+    /**
+     * @brief Collects one orientation of calibration data.
+     * 
+     * @return true if 12 orientations have been sampled.
+     * @return false if less than 12 orientations have been sampled.
+     */
     bool CollectCalibrationData(); 
     
-
+    /**
+     * @brief Resets the clalibration data and progress. MUST be run before calibration.
+     */
     void ResetCalibration();
 
+    /**
+     * @brief Loads calibration data from file
+     */
     void LoadCalibration();
 
+    /**
+     * @brief Resets alignment data and progress. MUST be run before alignment.
+     */
     void ResetAlignment();
 
+    /**
+     * @brief Loads alignment data from file
+     */
     void LoadAlignment();
 
+    /**
+     * @brief Enables the laser dot on the laser sensor
+     */
     void EnableLaser();
 
+    /**
+     * @brief Disables the laser dot on the laser sensor
+     */
     void DisableLaser();
 
-
+    /**
+     * @brief Updates the sensors and gets a reading from all sensors
+     * 
+     * @return Vector3f Heading [deg], Inclination [deg], Distance [m]
+     */
     Vector3f GetReading();
 };
 

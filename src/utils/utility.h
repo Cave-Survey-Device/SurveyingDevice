464c4a028b4029f8f24ab5b9b8ea8b3f0d23a9d3
christiankuhlmann <99804123+christiankuhlmann@users.noreply.github.com>
2023-04-30 18:31:45 +0100

    Added Doxygen comments

--- src/utils/utility.h
+++ src/utils/utility.h
@@ -8,6 +8,10 @@
 
 using namespace Eigen;
 
+/**
+ * @brief A node to be saved. Id is not needed but could be used later if the device to to be used standalone.
+ * 
+ */
 struct node
 {
     int id;
@@ -17,17 +21,57 @@ struct node
 };
 
 // Generate vector from current node back to base
+/**
+ * @brief Generates a vector from distance, heading, inclination
+ * TODO: maybe convert to HID like all other funcs, or change all funct to DHI.
+ * 
+ * @param distance 
+ * @param heading 
+ * @param inclination 
+ * @return Vector3f 
+ */
 Vector3f generate_vector(float distance, float heading, float inclination);
 
+/**
+ * @brief Pretty debug for disto
+ * 
+ * @param mode 
+ * @param str 
+ */
 void debug(unsigned int mode, const char* str);
+
+/**
+ * @brief Debug with functionality of printf. Very nice :).
+ * 
+ * @param mode 
+ * @param format 
+ * @param ... 
+ */
 void debugf(unsigned int mode, const char *format, ...);
 
-
+/**
+ * @brief Converts spherical data to cartesian data (H, I, D) to (X, Y, Z)
+ * 
+ * @param spherical Spherical data HID
+ * @return Vector3f Cartesian data XYZ
+ */
 Vector3f Cartesian(Vector3f spherical);
 
+/**
+ * @brief Converts cartesian data to cartesian data (X, Y, Z) to (H, I, D)
+ * 
+ * @param cartesian Cartesian data XYZ
+ * @return Vector3f Spherical data HID
+ */
 Vector3f Spherical(Vector3f cartesian);
 
-// heading, inclination, roll
+/**
+ * @brief Finds the orientation of the device from gravitational and magnetic data.
+ * 
+ * @param g Gravitationl data
+ * @param m Magnetic data
+ * @return Vector3f 
+ */
 Vector3f Orientation(Vector3f g, Vector3f m);
 
 #endif

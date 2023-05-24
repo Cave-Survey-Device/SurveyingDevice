0c7f82360bbf942c98f8eecbb2a5dc957ef81692
christiankuhlmann <99804123+christiankuhlmann@users.noreply.github.com>
2023-05-24 16:57:08 +0100

    removed deprecated code

--- lib/sensors_csd/accelerometer_csd.cpp
+++ lib/sensors_csd/accelerometer_csd.cpp
@@ -2,15 +2,11 @@
 
 Accelerometer::Accelerometer(InertialSensorConnection* sc) : InertialSensor(sc, &calibration_data(0,0), n_calib)
 {
-    // ref_calibration_data = calibration_data;
-    // new (&ref_calibration_data) Map<MatrixXf>(calibration_data.data(),calibration_data.rows(),calibration_data.cols());
-    //ref_calibration_data = calibrated_data;
-    //new (&ref_calibration_data) Map<Matrix<float,3,-1>>(&calibration_data(0,0),3,n_calib);
     static_assert(n_calib >= N_INERTIAL_ALIGNMENT,  "STATIC_ASSERT n_calib >= N_INERTIAL_ALIGNMENT");
     setID("acc");
 }
 
-Ref<MatrixXf> Accelerometer::getCalibData();
+Ref<MatrixXf> Accelerometer::getCalibData()
 {
   return calibration_data;
 }

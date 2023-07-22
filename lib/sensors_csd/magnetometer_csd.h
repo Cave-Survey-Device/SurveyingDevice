b77876762b6d78d4966e5810ee8a08a1dcf1924e
christiankuhlmann <99804123+christiankuhlmann@users.noreply.github.com>
2023-07-22 17:08:12 +0200

    Added docstrings

--- lib/sensors_csd/magnetometer_csd.h
+++ lib/sensors_csd/magnetometer_csd.h
@@ -6,20 +6,23 @@
 #include <ArduinoEigenDense.h>
 using namespace Eigen;
 
+/**
+ * @brief 
+ * 
+ */
 class Magnetometer: public InertialSensor
 {
 private:
-  const static int n_calib = 12*5; 
-  Matrix<float,3,n_calib> calibration_data;
-  //float calibration_data[3][n_calib];
+  const static int n_calib = 12*5; /** Number of columns in the calibration data matrix*/
+  Matrix<float,3,n_calib> calibration_data; /** Calibration data matrix*/
   
 public:
-  Magnetometer(InertialSensorConnection* sc);
-  int checkCalibrationProgress();
-  int getMagnetometerIndex(const Vector3f &sample);
-  void addCalibrationData();
+  Magnetometer(InertialSensorConnection* sc); /** Constructor for the Magnetometer object*/
+  int checkCalibrationProgress(); /** Checks the calibration progress of the magnetometer when using the standalone calibration*/
+  int getMagnetometerIndex(const Vector3f &sample); /** Gets the index of a sample in the magnetometer calibration array */
+  void addCalibrationData(); /** Collects a sample and and adds it to the magnetometer calibration Matrix when using standalone calibration*/
 
-  Ref<MatrixXf> getCalibData();
+  Ref<MatrixXf> getCalibData(); /** Gets the matrix of calibration data*/
 
 };
 

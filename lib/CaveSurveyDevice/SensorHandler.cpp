adb4bcb1231113f2704772027767db9dfdc844f1
christiankuhlmann <99804123+christiankuhlmann@users.noreply.github.com>
2023-12-09 13:24:56 +0000

    Implemented a number of funcations and fixed ref for sensors

--- lib/CaveSurveyDevice/SensorHandler.cpp
+++ lib/CaveSurveyDevice/SensorHandler.cpp
@@ -1,16 +1,72 @@
 #include "SensorHandler.h"
 #include <queue>
 
-SensorHandler::SensorHandler(Accelerometer &a, Magnetometer &m, Laser &l)
+// IMPORTANT: References are immutable and must be definied upon initialisation!
+SensorHandler::SensorHandler(Accelerometer &a, Magnetometer &m, Laser &l):acc(a), mag(m), las(l){}
+
+void SensorHandler::init()
 {
-    acc = a;
-    mag = m;
-    las = l;
+    acc.init();
+    mag.init();
+    las.init();
+
+    // Should be loadCalibration
+    resetCalibration();
 }
 
 Vector3f SensorHandler::getAccData() { return acc_data; }
 Vector3f SensorHandler::getMagData() { return mag_data; }
-Vector3f SensorHandler::getLasData() { return las_data; }
+float SensorHandler::getLasData() { return las_data; }
+
+void SensorHandler::resetCalibration()
+{
+    calib_parms.laser_inclination = 0;
+    calib_parms.laser_heading = 0;
+    calib_parms.Ra.setIdentity();
+    calib_parms.Rm.setIdentity();
+    calib_parms.ba.setZero();
+    calib_parms.bm.setZero();
+    calib_parms.Ralign.setIdentity();
+    calib_parms.inclination_angle = 0;
+}
+
+
+Vector3f SensorHandler::getCardan()
+{
+    return NumericalMethods::inertialToCardan(acc_data, mag_data);
+}
+Vector3f SensorHandler::getCartesian()
+{
+    return NumericalMethods::inertialToCartesian(acc_data, mag_data);
+
+}
+Vector3f SensorHandler::getFinalMeasurement()
+{
+    static Vector3f cartesian, cardan, laser_direction, target_location, out;
+     
+    // Apply inertial calibration
+    mag_data = calib_parms.Ralign * calib_parms.Rm * (mag_data - calib_parms.ba);
+    acc_data = calib_parms.Ra * (acc_data - calib_parms.ba);
+
+    // Apply laser calibration
+    cardan = NumericalMethods ::inertialToCardan(acc_data,mag_data);
+    cartesian = NumericalMethods::inertialToCartesian(acc_data,mag_data);
+    laser_direction = NumericalMethods::cardanToCartesian(Vector3f(
+        cardan(0) + calib_parms.laser_heading,
+        cardan(1) + calib_parms.laser_inclination,
+        0));
+
+    target_location = cartesian*DEVICE_LENGTH + las_data*laser_direction;
+
+    // Heading, Inclination, Distance
+    // Use of -ve to convert from RH cardan to LH heading and inclination
+    out << -NumericalMethods::cartesianToCardan(target_location), target_location.norm();
+
+    return out;
+
+}
+
+
 
 int SensorHandler::takeShot(const bool laser_reading)
 {
@@ -18,13 +74,13 @@ int SensorHandler::takeShot(const bool laser_reading)
     Vector<float,N_STABILISATION> norm_buffer;
     for (int i=0; i<N_STABILISATION; i++)
     {
-        norm_buffer(i) = acc.getReading().norm();
+        norm_buffer(i) = acc.getMeasurement().norm();
     }
 
     int i = 0;
     while (NumericalMethods::stDev(norm_buffer) > STDEV_LIMIT)
     {
-        norm_buffer(i%N_STABILISATION) = acc.getReading().norm();
+        norm_buffer(i%N_STABILISATION) = acc.getMeasurement().norm();
         i++;
 
         if (i > 1000) { return 1; }
@@ -36,21 +92,23 @@ int SensorHandler::takeShot(const bool laser_reading)
     acc_data << 0,0,0;
     for (int i=0; i<N_SHOT_SMAPLES; i++)
     {
-        mag_data += mag.getSingleSample();
-        acc_data += acc.getSingleSample();
+        mag_data += mag.getMeasurement();
+        acc_data += acc.getMeasurement();
     }
     mag_data /= N_SHOT_SMAPLES;
     acc_data /= N_SHOT_SMAPLES;
 
-    if (laser_reading) { las_data = las.getReading(); }
+    if (laser_reading) { las_data = las.getMeasurement(); }
 
     return 0;
 }
 
 Vector2i SensorHandler::getMagCalIndex(const Vector3f &m)
 {
+    static Vector2f indexf;
     static Vector2i index;
-    index << (RAD_TO_DEG * NumericalMethods::cartesianToCardan(m)).array().floor();
+    indexf << (RAD_TO_DEG * NumericalMethods::cartesianToCardan(m));
+    index << floor(indexf(0)), floor(indexf(1));
 
     // Heading ranges from 0 to 360 and inclination from -90 to 90
     index << floor(index(0) * (N_MAG_CAL_HEADING-1)/360), floor((index(0)+90) * (N_MAG_CAL_INCLINATION-1)/180);
@@ -66,7 +124,7 @@ int SensorHandler::collectMagCalibData()
 {
     // Collect data
     static Vector2i index;
-    mag_data = mag.getSingleSample();
+    mag_data = mag.getMeasurement();
     index = getMagCalIndex(mag_data);
 
     mag_calib_data.col(index(0)*N_MAG_CAL_INCLINATION+index(1)) = mag_data;
@@ -106,6 +164,7 @@ int SensorHandler::calibrateMagnetometer()
     int n_zeros = NumericalMethods::removeNullData(mag_calib_data);
     Vector<float,10> U = NumericalMethods::fitEllipsoid(mag_calib_data.block(0,0,3,N_MAG_CAL - n_zeros));
     NumericalMethods::calculateEllipsoidTransformation(U, calib_parms.Rm, calib_parms.bm);
+    return 0;
 }
 
 int SensorHandler::alignInertial()
@@ -120,7 +179,6 @@ int SensorHandler::alignInertial()
     NumericalMethods::calculateEllipsoidTransformation(Ua, calib_parms.Ra, calib_parms.ba);
 
     NumericalMethods::alignMagAcc(acc_align_data, mag_align_data,calib_parms.Ralign,calib_parms.inclination_angle);
-
     return 0;
 }
 

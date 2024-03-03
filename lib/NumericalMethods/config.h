1fafa373695b2fa88b303fbccb77b3815ae2d5de
christiankuhlmann <99804123+christiankuhlmann@users.noreply.github.com>
2024-03-03 17:10:07 +0000

    added comments

--- lib/NumericalMethods/config.h
+++ lib/NumericalMethods/config.h
@@ -1,10 +1,10 @@
 #ifndef NUMERICAL_METHODS_CONFIG_H
 #define NUMERICAL_METHODS_CONFIG_H
 
-const int N_ORIENTATIONS = 12;
-const int N_SAMPLES_PER_ORIENTATION = 20;
-const int N_ALIGN_MAG_ACC = N_ORIENTATIONS*N_SAMPLES_PER_ORIENTATION;
-const int N_LASER_CAL = 8;
-const float DEVICE_LENGTH = 0.2;
+const int N_ORIENTATIONS = 12; // Number of orientations used in static calibration
+const int N_SAMPLES_PER_ORIENTATION = 20; // Number of samples to be saved per orientation
+const int N_ALIGN_MAG_ACC = N_ORIENTATIONS*N_SAMPLES_PER_ORIENTATION; // Size of matrix required for static alignment data storeage
+const int N_LASER_CAL = 8; // Number of orientations used in alser calibration
+const float DEVICE_LENGTH = 0.2; // Length of device
 
 #endif

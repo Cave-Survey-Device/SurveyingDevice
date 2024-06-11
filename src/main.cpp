ab201af3cdb515beb736ca1d55eff29eee651ef5
christiankuhlmann <99804123+christiankuhlmann@users.noreply.github.com>
2024-06-11 23:04:59 +0100

    working with buttons

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,9 @@
 #include "SCA3300SensorConnection.h"
 #include "LDK2MSensorConnection.h"
 #include "utils.h"
-
+#include "FreeRTOS.h"
+#include "BLE.h"
 #include <ArduinoJson.h>
-
 
 static RM3100 rm3100;
 static SCA3300 sca3300;
@@ -16,242 +16,161 @@ static SCA3300SensorConnection sc_accelerometer(sca3300);
 static RM3100SensorConnection sc_magnetometer(rm3100);
 static LDK2MSensorConnection sc_laser(ldk2m);
 
-static SensorHandler sh(sc_accelerometer, sc_magnetometer, sc_laser);
-
 static OLED oled;
 
-String strcmd;
-const char* cmd;
-void serialEvent()
-{
-  // cmd = Serial.parseInt();
-  Serial << "\nserialEvent\n";
-  strcmd = Serial.readStringUntil('\n');
-  Serial << "strcmd: " << strcmd << "\n";
-}
-
-// void addJSONArr(const char * name, JsonArray &arr, Ref<MatrixXf> data){
-//     for 
-// }
+static SensorHandler sh(sc_accelerometer, sc_magnetometer, sc_laser);
+// static BLEHandler bh;
 
 typedef Matrix<float, Dynamic, Dynamic, RowMajor> RowMatrixXf;
 static StaticJsonDocument<60480> root;
 static float eig_arr[3][N_ALIGN_MAG_ACC];
 static float las_arr[3][N_LASER_CAL];
 
+static unsigned int current_shot_ID = 0;
+static unsigned int current_file_ID = 0;
+static ShotData current_shot_data;
 
-int n;
-Vector3f orientation;
-void mainloop(void *){
-while(true){
-    cmd = strcmd.c_str();
+int begin()
+{
+    Serial.begin(115200);
+    sh.init();
+    return 0;
+}
+int takeShot()
+{
+    Serial.print("Taking shot...\n");
+    if (!sh.takeShot())
+    {
+        Serial.print("Saving shot...\n");
+        current_shot_data = sh.getShotData();
+        saveShotData(current_shot_data,current_file_ID);
+        // Read data after saving to get the ID assigned to it
+        readShotData(current_shot_data,current_file_ID);
+        // Write data to BLE
+        // bh.shared_ble_data.write_data(&current_shot_data);
+    }
+    return 0;
+}
 
-    if (!strcmp(cmd, "1")) { // Collect static alignment data
-
-        Serial << "Collect static calibration data...\n";
-        n = sh.collectStaticCalibData();
-        Serial << "Progress: " << n << "\n";
-        if (n >= N_ORIENTATIONS){ sh.calibrate();}
-        strcmd = "";
-
-    } else if (!strcmp(cmd, "3")) { // Collect laser alignment data
-
-        Serial << "Collect laser calibration data\n";
-        n = sh.collectLaserCalibData();
-        Serial << "Progress: " << n << "\n";
-        strcmd = "";
-        delay(100);
-        ldk2m.toggleLaser(true);
-
-    } else if (!strcmp(cmd, "align")) { // align the device
-
-        Serial << "align\n";
-        sh.align();
-        strcmd = "";
-
-    } else if (!strcmp(cmd, "calibrate")) { // calibrate the device
-
-        Serial << "calibrate\n";
-        sh.calibrate();
-        strcmd = "";
-
-    } else if (!strcmp(cmd, "shot")) { // Collect inertial alignment data
-
-        Serial << "shot\n";
-        sh.takeShot();
-
-        ShotData sd = sh.getShotData();
-        Vector3f HIR = sh.getCardan();
-
-        Serial.printf("Final measurement: %f %f %f\n", sd.HIR(0), sd.HIR(1), sd.d);
-        Serial.printf("Final vec: %f %f %f\n", sd.v(0), sd.v(1), sd.v(2));
-        // Serial.printf("Magnetic measurement: %f %f %f\n",sd.m(0),sd.m(1),sd.m(2));
-        // Serial.printf("Gravitational measurement: %f %f %f\n",sd.g(0),sd.g(1),sd.g(2));
-        // Serial.printf("Laser measurement: %f\n", sd.d);
-
-        strcmd = ""; 
-
-
-    } else if (!strcmp(cmd, "erase")) { // Collect inertial alignment data
-
-        Serial << "erase\n";
-        sh.eraseFlash();
-        strcmd = "";
-    } else if (!strcmp(cmd, "flashstats")) { // Collect inertial alignment data
-
-        Serial << "flashstats\n";
-        sh.getFlashStats();
-        strcmd = "";
-
-    } else if (!strcmp(cmd, "las")) { // Collect inertial alignment data
-
-        Serial << "las\n";
-        ldk2m.toggleLaser();
-        strcmd = "";
-
-    } else if (!strcmp(cmd, "2")) { // Output calibration data and parameters as JSON to serial
-
-        DeviceCalibrationParameters parm_data = sh.getCalibParms();
-        
-        root.clear();
-
-        JsonArray jarr;
-        MatrixXf static_acc_samples, static_mag_samples, laser_acc_samples, laser_mag_samples;
-
-        JsonArray acc_data = root.createNestedArray("static_acc_samples");
-        static_acc_samples = sh.getStaticCalibData().acc_data;
-        Map<RowMatrixXf>(&eig_arr[0][0], 3, N_ALIGN_MAG_ACC) = static_acc_samples; 
-        copyArray(eig_arr,acc_data);
-        Serial.print("static_acc_samples\n");
-        // serializeJson(acc_data,Serial);
-
-        JsonArray mag_data = root.createNestedArray("static_mag_samples");
-        static_mag_samples = sh.getStaticCalibData().mag_data;
-        Map<RowMatrixXf>(&eig_arr[0][0], 3, N_ALIGN_MAG_ACC) = static_mag_samples; 
-        copyArray(eig_arr,mag_data);
-        Serial.print("static_mag_samples\n");
-        // serializeJson(mag_data,Serial);
-
-        JsonArray las_acc_data = root.createNestedArray("laser_acc_samples");
-        laser_acc_samples = sh.getLaserCalibData().acc_data;
-        Map<RowMatrixXf>(&las_arr[0][0], 3, N_LASER_CAL) = laser_acc_samples; 
-        copyArray(las_arr,las_acc_data);
-        Serial.print("laser_acc_samples\n");
-        // serializeJson(las_acc_data,Serial);
-
-        JsonArray las_mag_data = root.createNestedArray("laser_mag_samples");
-        laser_mag_samples = sh.getLaserCalibData().mag_data;
-        Map<RowMatrixXf>(&las_arr[0][0], 3, N_LASER_CAL) = laser_mag_samples; 
-        copyArray(las_arr,las_mag_data);
-        Serial.print("laser_mag_samples\n");
-        serializeJson(las_mag_data,Serial);
-        Serial << "\n\n";
-        
-
-
-        
-        // JsonObject parms = root.createNestedObject("parms");
-        JsonArray Ra_static = root.createNestedArray("Ra_static");
-        JsonArray ba_static = root.createNestedArray("ba_static");
-        JsonArray Rm_static = root.createNestedArray("Rm_static");
-        JsonArray bm_static = root.createNestedArray("bm_static");
-        JsonArray Ra_laser = root.createNestedArray("Ra_laser");
-        JsonArray Rm_laser = root.createNestedArray("Rm_laser");
-        JsonArray Rm_align = root.createNestedArray("Rm_align");
-
-
-        float vec3fdata[3];
-        float mat3fdata[3][3];
-
-        Map<RowMatrixXf>(&mat3fdata[0][0], 3, 3) = parm_data.Ra_cal; 
-        copyArray(mat3fdata,Ra_static);
-        Map<Vector3f>(&vec3fdata[0], 3) = parm_data.ba_cal; 
-        copyArray(vec3fdata,ba_static);
-        
-
-        Map<RowMatrixXf>(&mat3fdata[0][0], 3, 3) = parm_data.Rm_cal; 
-        copyArray(mat3fdata,Rm_static);
-        Map<Vector3f>(&vec3fdata[0], 3) = parm_data.bm_cal; 
-        copyArray(vec3fdata,bm_static);
-
-
-        Map<RowMatrixXf>(&mat3fdata[0][0], 3, 3) = parm_data.Ra_las; 
-        copyArray(mat3fdata,Ra_laser);
-
-        Map<RowMatrixXf>(&mat3fdata[0][0], 3, 3) = parm_data.Rm_las; 
-        copyArray(mat3fdata,Rm_laser);
-
-
-        Map<RowMatrixXf>(&mat3fdata[0][0], 3, 3) = parm_data.Rm_align; 
-        copyArray(mat3fdata,Rm_align);
-
-        serializeJson(root,Serial);
-
-        Serial << "Inclination angle: " << parm_data.inclination_angle << "\n";
-   
-
-        Serial << "\n\n";
-        strcmd = "";
-
-    } else if (!strcmp(cmd, "save")) { // Save calibration parameters
-        Serial << "Save\n";
-        sh.saveCalibration();
-        strcmd = "";
-
-
-    } else if (!strcmp(cmd, "load")) { // Load calibration parameters
-        Serial << "Load\n";
-        sh.loadCalibration();
-        strcmd = "";
-
-
-    } else if (!strcmp(cmd, "reset")) { // Reset calibration parameters
-        Serial << "Reset\n";
-        sh.resetCalibration();
-        strcmd = "";
+int removeRecentCalib()
+{
+    if (sh.getCalibProgress(true) < N_ORIENTATIONS)
+    {
+        sh.removePrevCalib(true);
     } else {
-        // Serial << "Update OLED\n";
-        delay(50);
-        // sh.takeShot(false,false);
-        sh.update();
-        orientation = sh.getCardan();
-        // oled.Clino(-RAD_TO_DEG * orientation(1));
-        // oled.Compass(-RAD_TO_DEG * orientation(0));
-    }  
-
-    cmd = "";
-}
+        sh.removePrevCalib(false);
+    }
+    return 0;
 }
 
+int nextCalib(){
+    // Check static calib progress, if > N_ORIENTATIONS, return N_ORIENTATIONS + laser calib progress
+    // Otherwise return static calib progress
+    if (sh.getCalibProgress(true) < N_ORIENTATIONS)
+    {
+        return sh.collectStaticCalibData();
+    } else {
+        return N_ORIENTATIONS + sh.collectLaserCalibData();
+    }
+    return 0;
+}
 
-void setup() {
+int prevData()
+{
+    if (current_shot_ID - 1 >= 0) { current_shot_ID -= 1; }
+    readShotData(current_shot_data,current_file_ID,current_shot_ID);
+    return 0;
+}
+
+int nextData()
+{
+    static unsigned int counter;
+    static char fname[FNAME_LENGTH];
+    getFileName(current_file_ID,fname);
+    getCounter(current_file_ID, counter);    
+
+    if (current_shot_ID + 1 <= counter ) { current_shot_ID += 1; }
+    readShotData(current_shot_data,current_file_ID,current_shot_ID);
+    return 0;
+}
+
+int laserOn()
+{
+    sc_laser.toggleLaser(true);
+    return 0;
+}
+
+int laserOff()
+{
+    sc_laser.toggleLaser(false);
+    return 0;
+}
+
+int historyScrollUp(){
+    return 0;
+}
+
+int historyScrollDown(){
+    return 0;
+}
+
+void init()
+{
     Serial.begin(115200);
     Serial << "Begin beaning...\n\n";
     sh.init();
 
     Serial << "SensorHandler initialised, initialising OLED...\n\n";
-    //oled.Initialise();
+    oled.Initialise();
 
     Serial << "OLED initialised, testing magnetometer and accelerometer...\n";
     Serial.printf("Mag data: %f %f %f\n",sc_magnetometer.getMeasurement()(0),sc_magnetometer.getMeasurement()(1),sc_magnetometer.getMeasurement()(2));
     Serial.printf("Aacc data: %f %f %f\n",sc_accelerometer.getMeasurement()(0),sc_accelerometer.getMeasurement()(1),sc_accelerometer.getMeasurement()(2));
 
     Serial << "Initialisation succesful, starting tasks...\n\n";
-
-    TaskHandle_t hardware_handle;
-    xTaskCreatePinnedToCore(
-        mainloop, /* Function to implement the task */
-        "main", /* Name of the task */
-        50000,  /* Stack size in words */
-        NULL,  /* Task input parameter */
-        tskIDLE_PRIORITY ,  /* Priority of the task */
-        &hardware_handle,  /* Task handle. */
-        0); /* Core where the task should run */
-
 }
 
 
+/**
+ * @brief Setup task for Arduino framework
+ * 
+ */
+void setup()
+{
+    init();
+    delay(500);
+    xTaskCreatePinnedToCore(
+        eventhandler, /* Function to implement the task */
+        "eventhandler", /* Name of the task */
+        10000,  /* Stack size in words */
+        NULL,  /* Task input parameter */
+        3 ,  /* Priority of the task */
+        &eventhandler_task,  /* Task handle. */
+        0); /* Core where the task should run */
+
+    delay(500);
+    xTaskCreatePinnedToCore(
+        computefunc, /* Function to implement the task */
+        "computefunc", /* Name of the task */
+        100000,  /* Stack size in words */
+        NULL,  /* Task input parameter */
+        2 ,  /* Priority of the task */
+        &computefunc_task,  /* Task handle. */
+        0); /* Core where the task should run */
+
+    // delay(500);
+    // xTaskCreatePinnedToCore(
+    //     blehandler, /* Function to implement the task */
+    //     "blehandler", /* Name of the task */
+    //     10000,  /* Stack size in words */
+    //     NULL,  /* Task input parameter */
+    //     1 ,  /* Priority of the task */
+    //     &BLEhandler_task,  /* Task handle. */
+    //     0); /* Core where the task should run */
+
+
+
+        init_interrupts();
+}
+
 void loop(){}
-
-
-

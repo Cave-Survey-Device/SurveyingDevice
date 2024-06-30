7fa28555fb7951d82e416bd31902e4fe2c5e0dcc
christiankuhlmann <99804123+christiankuhlmann@users.noreply.github.com>
2024-06-30 14:09:29 +0100

    Added compass graphics - Need to fix stoboscopic effect - Need to tidy code - Need to add arrows to pointed direction

--- lib/OLED/OLED.cpp
+++ lib/OLED/OLED.cpp
@@ -1,56 +1,161 @@
 // OLED.cpp
 #include "OLED.h"
 
+using namespace OLED;
 
-
-OLED::OLED() {
+Point::Point(int px, int py)
+{
+	x = px;
+	y = py;
 }
 
-void OLED::Initialise() {
-  display.begin(0x3D, true); // Address 0x3D default
-  display.setTextSize(2); // 2:1 pixel scale
-  display.setTextColor(SH110X_WHITE); // Tells display to draw white text
-  display.setTextColor(SH110X_WHITE, SH110X_BLACK); //sets the text background colour to black to overwrite existing numbers, prevents flicker. 
-  //display.setContrast (0); // dim display (optional)
-  display.clearDisplay();
-  display.display();
+OLED::Point OLED::rotatePoint(const OLED::Point p, const int cx, const int cy , const float rads)
+{
+	return Point(	std::round(cos(rads) * (p.x - cx) - sin(rads) * (p.y - cy) + cx),
+                  	std::round(sin(rads) * (p.x - cx) + cos(rads) * (p.y - cy) + cy));
 }
 
-void OLED::Distance(double distance) {
+DisplayHandler::DisplayHandler() {
+}
+
+void DisplayHandler::Initialise() {
+	display.begin(0x3D, true); // Address 0x3D default
+	display.setTextSize(2); // 2:1 pixel scale
+	display.setTextColor(SH110X_WHITE); // Tells display to draw white text
+	display.setTextColor(SH110X_WHITE, SH110X_BLACK); //sets the text background colour to black to overwrite existing numbers, prevents flicker. 
+	//display.setContrast (0); // dim display (optional)
+	display.clearDisplay();
+	display.display();
+}
+
+void DisplayHandler::drawCompass() {
+	display.fillRect(0, 0, SCREEN_WIDTH, 28, SH110X_WHITE);
+	display.drawLine(cx-line_length,cy, cx+line_length, cy,SH110X_WHITE);
+	display.drawLine(cx,cy-line_length, cx, cy+line_length,SH110X_WHITE);
+
+	// North Triangle
+	display.fillTriangle( 	cx,cy+line_length,
+							cx-5,cy+line_length-5,
+							cx+5,cy+line_length-5,
+							SH110X_WHITE);
+
+	// South Triangle
+	display.fillTriangle( 	cx,cy-line_length,
+							cx-5,cy-line_length+5,
+							cx+5,cy-line_length+5,
+							SH110X_WHITE);
+
+	// East Triangle
+	display.fillTriangle( 	cx+line_length,cy,
+							cx+line_length-5,cy-5,
+							cx+line_length-5,cy+5,
+							SH110X_WHITE);
+
+  // West Triangle
+	display.fillTriangle( 	cx-line_length,cy,
+							cx-line_length+5,cy-5,
+							cx-line_length+5,cy+5,
+							SH110X_WHITE);
+}
+
+void DisplayHandler::drawCompassDirection(CompassDirection direction)
+{
+	int end_x,end_y;
+	float angle;
+	end_x = cx;
+	end_y = line_length;
+	switch(direction)
+	{
+		case NORTH:
+		angle = 0;
+		// end_x = cx;
+		// end_y = cy+line_length;
+		break;
+
+		case NORTH_EAST:
+		angle = M_PI_4;
+		// end_x = cx+line_length/M_SQRT2;
+		// end_y = cy+line_length/M_SQRT2;
+		break;
+
+		case EAST:
+		angle = 2*M_PI_4;
+		// end_x = cx;
+		// end_y = cy+line_length;
+		break;
+
+		case SOUTH_EAST:
+		angle = 3*M_PI_4;
+		// end_x = cx+line_length/M_SQRT2;
+		// end_y = cy-line_length/M_SQRT2;
+		break;
+
+		case SOUTH:
+		angle = 4*M_PI_4;
+		// end_x = cx;
+		// end_y = cy-line_length;
+		break;
+
+		case SOUTH_WEST:
+		angle = 5*M_PI_4;
+		// end_x = cx-line_length/M_SQRT2;
+		// end_y = cy-line_length/M_SQRT2;
+		break;
+
+		case WEST:
+		angle = 6*M_PI_4;
+		// end_x = cx;
+		// end_y = cy-line_length;
+		break;
+
+		case NORTH_WEST:
+		angle = 7*M_PI_4;
+		// end_x = cx-line_length/M_SQRT2;
+		// end_y = cy+line_length/M_SQRT2;
+		break;
+	}
+
+	Point p_end(end_x,end_y);
+	Point p_rotated = rotatePoint(p_end,cx,cy,angle);
+
+	display.drawLine(cx, cy, p_rotated.x, p_rotated.y,SH110X_WHITE);
+}
+
+void DisplayHandler::Distance(double distance) {
   display.fillRect(70, 30, 58, 98, SH110X_BLACK); //clears the old distance value
   display.setTextSize(3);
   display.setCursor(0, 34);
   display.print(distance);
   display.println("m");
-  display.display();
+  // display.display();
 }
 
-void OLED::Compass(double compass) {
+void DisplayHandler::Compass(double compass) {
   display.fillRect(70, 70, 58, 58, SH110X_BLACK); //clears the old compass value
   display.setTextSize(3);
   display.setCursor(0, 70);
   display.print(compass);
   display.print((char)247); // degree symbol 
-  display.display();
+  // display.display();
 }
 
-void OLED::Clino(double clino) {
+void DisplayHandler::Clino(double clino) {
   display.fillRect(70, 106, 58, 22, SH110X_BLACK); //clears the old clino value
   display.setTextSize(3);
   display.setCursor(0, 106);
   display.print(clino);
   display.print((char)247); // degree symbol 
-  display.display();
+  // display.display();
 }
 
-void OLED::Sensor_cal_status(int sensor_status) {
+void DisplayHandler::Sensor_cal_status(int sensor_status) {
   display.setTextSize(2);
   display.setCursor(23, 4);
   display.print(sensor_status);
-  display.display();
+  // display.display();
 }
 
-void OLED::Blutooth(bool ble_status)
+void DisplayHandler::Blutooth(bool ble_status)
 {
   // insert switch case to update
   const unsigned char PROGMEM Bluetooth_icon[] = {
@@ -72,7 +177,7 @@ void OLED::Blutooth(bool ble_status)
   }
 }
 
-void OLED::Battery(int batt_percentage) {
+void DisplayHandler::Battery(int batt_percentage) {
   display.drawRect(90, 0, 32, 15, SH110X_WHITE);
   display.drawRect(122, 4, 3, 6, SH110X_WHITE);
   batt_level = (batt_percentage/100.00)*32;
@@ -85,6 +190,10 @@ void OLED::Battery(int batt_percentage) {
   display.display();
 }
 
-void OLED::clearDisplay() {
+void DisplayHandler::clearDisplay() {
    display.clearDisplay();
+}
+
+void DisplayHandler::update() {
+   display.display();
 }
--- lib/OLED/OLED.h
+++ lib/OLED/OLED.h
@@ -2,15 +2,48 @@
 #ifndef OLED_H
 #define OLED_H
 
+
+
+
+
 #include <Arduino.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
 
-class OLED {
+namespace OLED
+{
+
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 128 // OLED display height, in pixels
+#define OLED_RESET -1     // can set an oled reset pin if desired
+#define TOP_BAR_HEIGHT 28
+
+struct Point
+{
+  Point(int px, int py);
+  int x, y;
+};
+
+Point rotatePoint(const Point p, const int cx, const int cy , const float rads);
+
+enum CompassDirection
+{
+  NORTH,
+  NORTH_EAST,
+  EAST,
+  SOUTH_EAST,
+  SOUTH,
+  SOUTH_WEST,
+  WEST,
+  NORTH_WEST
+};
+
+
+class DisplayHandler {
   public:
 
     int rand_no;
-    OLED();
+    DisplayHandler();
     void Distance(double distance);
     void Compass(double compass);
     void Clino(double clino);
@@ -19,7 +52,17 @@ class OLED {
     void Battery(int batt_percentage);
     void Initialise();
     void clearDisplay();
+    void update();
     
+
+    void displayTopBar(bool bluetooth, int battery_level, int status);
+    void drawCompass();
+    void drawCompassDirection(CompassDirection direction);
+    void displayOrientation();
+    void displayShot();
+    void displayCalibSaveYN();
+    void displayData();
+
     double batt_voltage;
     int batt_level;
     int batt_percentage;
@@ -30,11 +73,15 @@ class OLED {
 
   private:
     
-    #define SCREEN_WIDTH 128  // OLED display width, in pixels
-    #define SCREEN_HEIGHT 128 // OLED display height, in pixels
-    #define OLED_RESET -1     // can set an oled reset pin if desired
+    const int line_length = 40;
+    const int cx = SCREEN_WIDTH/2;
+    const int cy = TOP_BAR_HEIGHT + (SCREEN_HEIGHT - TOP_BAR_HEIGHT)/2;
+
     Adafruit_SH1107 display = Adafruit_SH1107(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET, 1000000, 100000);
     //Adafruit_SSD1327 display = Adafruit_SSD1327(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET, 1000000);
+
+};
+
 
 };
 
